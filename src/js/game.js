524a42f8ee04e7f7cf826db850f1f8d32c1f5a07
Spencer Dixon <spencercdixon@gmail.com>
2015-07-11 13:07:25 -0400

    Player is now using physics engine

--- src/js/game.js
+++ src/js/game.js
@@ -5,23 +5,37 @@
 
   Game.prototype = {
     create: function () {
+      this.physics.startSystem(Phaser.Physics.ARCADE);
       this.input.onDown.add(this.onInputDown, this);
       this.input.onDown.add(this.drawPlayer, this);
       this.player = this.add.sprite(32, this.world.height - 150, 'dude');
+      this.physics.arcade.enable(this.player);
+      debugger;
       this.cursors = this.input.keyboard.createCursorKeys();
     },
 
     update: function () {
-      // this.input.keyboard..x += 5;
-      if (this.cursors.right.isDown) {
-        this.player.x += 5;
-      }
+      this.movePlayer();
     },
+
     onInputDown: function () {
       // this.game.state.start('menu');
     },
+
     drawPlayer: function() {
       // this.player = new Player();
+    },
+
+    movePlayer: function() {
+      if (this.cursors.right.isDown) {
+        this.player.body.x += 5;
+      } else if (this.cursors.left.isDown) {
+        this.player.body.x -= 5;
+      } else if (this.cursors.up.isDown) {
+        this.player.body.y -= 5;
+      } else if (this.cursors.down.isDown) {
+        this.player.body.y += 5;
+      }
     }
   };
 
--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ window.addEventListener('load', function () {
   game.state.add('boot', ns.Boot);
   game.state.add('preloader', ns.Preloader);
   game.state.add('menu', ns.Menu);
+  game.state.add('player', ns.Player);
   game.state.add('game', ns.Game);
   /* yo phaser:state new-state-files-put-here */
   game.state.start('boot');
